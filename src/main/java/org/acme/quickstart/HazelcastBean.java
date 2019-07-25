7ba082234cec193ccd1d934d5b438b60d671b202
enozcan <44522401+enozcan@users.noreply.github.com>
2019-07-25 14:40:38 +0300

    Add client networking address section

--- src/main/java/org/acme/quickstart/HazelcastBean.java
+++ src/main/java/org/acme/quickstart/HazelcastBean.java
@@ -14,7 +14,11 @@ public class HazelcastBean {
 
     @Produces
     HazelcastInstance createInstance() {
+        Scanner console = new Scanner(System.in);
+        System.out.print("Enter networking address: ");
+        String ip = console.nextLine();
         ClientConfig clientConfig = new ClientConfig();
+        clientConfig.getNetworkConfig().addAddress(ip);
         return HazelcastClient.newHazelcastClient(clientConfig);
     }
 }
